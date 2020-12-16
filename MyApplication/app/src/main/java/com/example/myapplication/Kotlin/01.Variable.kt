a7907528b83c485ad0d303cb787ee7d1843a3ff0
KHJ <jinkh0129@naver.com>
2020-12-16 11:27:48 +0900

    default files

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Kotlin/01.Variable.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.Kotlin
+
+// 01. Variable ->  변수
+
+/*
+변수는 상자라고 할 수 있다.
+<변수를 선언하는 방법>
+val/var 변수명(상자) = 값(넣고 싶은 것)
+val : value(변하지 않는 값)
+var : variable(변할 수 있는 값)
+ */
+
+fun main(args:Array<String>){
+
+}
