b70c969a7f5c0e44e70b20d2aefa60e0c0d693c9
KHJ <jinkh0129@naver.com>
2020-12-23 10:35:51 +0900

    fastcampus

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.linearlayout01)
         // activity_main은 에뮬레이터에 띄우고 싶은 화면을 의미한다.
         // 그린 화면을 띄우고 싶으면 만든 xml파일 이름을 적어주면 된다.
     }
